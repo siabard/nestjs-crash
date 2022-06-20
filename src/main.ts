447098cc20c4aacc79386e2d0f4106e260338497
Yeonho Jang <siabard@gmail.com>
2022-06-20 16:13:48 +0900

    Validation Pipe 와 class-validator를 이용한 파라미터 점검

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+  
   const config = new DocumentBuilder().setTitle('Nest API').setDescription('the description').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsAlphanumeric, MaxLength } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty()
+    @IsAlphanumeric()
+    @MaxLength(20)
     name: string;
 
-    @ApiProperty({required: false})
-    age?: number;
 }

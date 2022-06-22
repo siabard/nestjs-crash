29ff6fef7211d81debbcea4117bf713b3bf825ea
Yeonho Jang <siabard@gmail.com>
2022-06-23 07:56:50 +0900

    typeorm  연결기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { config } from '../ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [TypeOrmModule.forRoot(config), UsersModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })

e1fbfb921e1eba054eef192cd8f7ba64cc6723bd
Yeonho Jang <siabard@gmail.com>
2022-07-04 12:50:25 +0900

    Add JWT dependencies.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor (private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(username: string, password: string): Promise<any> {
-        const user = await this.usersService.findOne(username);
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
 
-        if(user && user.password === password) {
-            const {password, username, ...rest} = user;
-            return rest;
-        }
-
-        return null;
+    if (user && user.password === password) {
+      const { password, username, ...rest } = user;
+      return rest;
     }
+
+    return null;
+  }
+
+  async login(users: any) {
+    const payload = { name: users.name, sub: users.id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
